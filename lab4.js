a45a9c1238fb9811aa0b4df0a3b3f0b86c81a2bf
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2026-07-07 09:45:37 +0600

    Lab 4 codes

---
+++ lab4.js
@@ -0,0 +1,22 @@
+function onSubmitForm(){
+    // alert("Please click ok1 to close");
+    //  alert("Please click ok2 to close");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    console.log({name, email});
+    if(!name){
+        document.getElementById("nameError").innerHTML = "Name is required";
+    }else if (name.length<6){
+      document.getElementById("nameError").innerHTML = "Name should be at least 6 char long";  
+    }
+    if(!email){
+        document.getElementById("emailError").innerHTML = "Email is required";
+    }else if (email.length<10){
+      document.getElementById("emailError").innerHTML = "Email should be at least 10 char long";  
+    }
+    return false;
+}
+
+function handleClick(){
+    alert("hehehehe")
+}
